55136e7c48a7e1668fd1bce256e7cd124effc885
datnt19213 <91647943+datnt19213@users.noreply.github.com>
2023-04-08 12:44:10 +0700

    repush

--- view/profile.php
+++ view/profile.php
@@ -19,7 +19,7 @@
           <p class="fb-create-tittle">Your Profile</p>
         </div>
         <div class="pr-form-input">
-          <input type="text" name="" placeholder="Your name" class="fb-end-date" />
+          <input type="text" name="" placeholder="Your name" class="fb-end-date" readonly style="pointer-events: none" />
           <div class="fb-end-date">
             <p class="fb-end-date-label">Birthday</p>
             <input type="date" name="" id="inputDate" class="fb-end-date-input" />
@@ -27,7 +27,7 @@
         </div>
         <div class="pr-form-input">
           <input type="tel" placeholder="Your phone number" name="" class="fb-end-date" />
-          <select name="" id="" class="option-list-cate" required>
+          <select name="" id="" class="option-list-cate" required readonly style="pointer-events: none">
             <option value="0" selected>Your position</option>
             <option value="">position 1</option>
             <option value="">position 2</option>
@@ -37,10 +37,10 @@
             <option value="">position 6</option>
           </select>
         </div>
-        <input type="email" placeholder="Your email" name="" class="fb-end-date" />
+        <input type="email" placeholder="Your email" name="" class="fb-end-date" readonly style="pointer-events: none" />
         <input type="text" placeholder="Address" name="" class="fb-end-date" />
         <div class="pr-form-input">
-          <select name="" id="" class="option-list-cate" required>
+          <select name="" id="" class="option-list-cate" required readonly style="pointer-events: none">
             <option value="0" selected>Your department</option>
             <option value="">Your department 1</option>
             <option value="">Your department 2</option>
