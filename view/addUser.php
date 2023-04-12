a465fe74bc78b3e3255752b1ad714a6634ec068d
phanthianhthu <thuptagcc19106@fpt.edu.vn>
2023-04-12 18:32:46 +0700

    update addUser

--- view/addUser.php
+++ view/addUser.php
@@ -17,9 +17,9 @@ include_once "../data/connection.php";
   <div class="form-wrapper">
     <form action="" id="form" class="form-right" style="width: 100%;" method="POST">
       <p class="title-right">Add User</p>
-      <input type="email" class="input-username" name="email" placeholder="Email" required />
-      <input type="password" class="input-password" name="pass" placeholder="Password" required />
-      <input type="text" class="input-password" name="fullName" placeholder="Fullname" required />
+      <input type="email" class="input-username" id="add_email" name="add_email" placeholder="Email" required />
+      <input type="password" class="input-password" id="add_pass" name="add_pass" placeholder="Password" required />
+      <input type="text" class="input-password" id="fullName" name="fullName" placeholder="Fullname" required />
       <div class="select-data-to-submit" style="gap: 0; height:fit-content;">
 
         <div class="input-password" style="position: relative;">
@@ -29,7 +29,7 @@ include_once "../data/connection.php";
         <?php
         $addUserPosLists = mysqli_query($conn, "SELECT * FROM position_tb");
         ?>
-        <select name="addU_position" id="" class="input-password" required style="background-color: #fff; color: var(--dark-blue)">
+        <select name="addU_position" id="addU_position" class="input-password" required style="background-color: #fff; color: var(--dark-blue)">
           <option value="" selected>Select Position</option>
           <?php
           while ($addUserPosListRow = mysqli_fetch_array($addUserPosLists, MYSQLI_ASSOC)) {
@@ -42,7 +42,7 @@ include_once "../data/connection.php";
         <?php
         $addUserDepLists = mysqli_query($conn, "SELECT * FROM department_tb");
         ?>
-        <select name="addU_department" id="" class="input-password" required style="background-color: #fff;color: var(--dark-blue); margin-bottom: 0.5%">
+        <select name="addU_department" id="addU_department" class="input-password" required style="background-color: #fff;color: var(--dark-blue); margin-bottom: 0.5%">
           <option value="" selected>Select Deparment</option>
           <?php
           while ($addUserDepListRow = mysqli_fetch_array($addUserDepLists, MYSQLI_ASSOC)) {
@@ -56,42 +56,41 @@ include_once "../data/connection.php";
       </div>
       <div style="display: flex;">
         <label for="" class="checkbox-label">
-          <input type="radio" class="check-box-terms" name="roleUser" value="0" />
+          <input type="radio" class="check-box-terms" id="roleUser" name="roleUser" value="0" />
           <p class="terms-check">Staff</p>
         </label>
         <label for="" class="checkbox-label">
-          <input type="radio" class="check-box-terms" name="roleUser" value="1" />
+          <input type="radio" class="check-box-terms" id="roleUser" name="roleUser" value="1" />
           <p class="terms-check">QA-coordinator</p>
         </label>
         <label for="" class="checkbox-label">
-          <input type="radio" class="check-box-terms" name="roleUser" value="2" />
+          <input type="radio" class="check-box-terms" id="roleUser" name="roleUser" value="2" />
           <p class="terms-check">QA-master</p>
         </label>
         <label for="" class="checkbox-label">
-          <input type="radio" class="check-box-terms" name="roleUser" value="3" />
+          <input type="radio" class="check-box-terms" id="roleUser" name="roleUser" value="3" />
           <p class="terms-check">Admin</p>
         </label>
       </div>
-      <button type="submit" class="login-btn" name="submit">Add</button>
+      <button type="submit" class="login-btn" id="submit" name="submit">Add</button>
     </form>
   </div>
-</body>
 <?php
 if (isset($_POST['submit'])) {
   // Lấy dữ liệu từ biểu mẫu
-  $email = $_POST['email'];
-  $password = $_POST['pass'];
+  $add_email = $_POST['add_email'];
+  $add_Pass = $_POST['add_pass'];
   $fullName = $_POST['fullName'];
   $dob_addUser = $_POST['dob_addUser'];
   $addU_position = $_POST['addU_position'];
   $addU_department = $_POST['addU_department'];
   $roleUser = $_POST['roleUser'];
 
-  $pass = md5($password);
+  $pass = md5($add_Pass);
   $dob = date('Y-m-d', $dob_addUser);
   // Thực hiện truy vấn chèn dữ liệu
-  $insertUserQuery = mysqli_query($conn, "INSERT INTO user_tb (email, password, fullname, dob, position_id , deparment_id , state_code)
-      VALUES ('$email', '$pass', '$fullName', '$dob', '$addU_position', '$addU_department', '$roleUser')");
+  $insertUserQuery = mysqli_query($conn, "INSERT INTO user_tb (email,password,fullname,dob,position_id,deparment_id,state_code)
+      VALUES ('$add_email', '$pass', '$fullName', '$dob', '$addU_position', '$addU_department', '$roleUser')");
 
   // Thực thi truy vấn và kiểm tra xem liệu có thành công hay không
   if ($insertUserQuery) {
@@ -101,5 +100,6 @@ if (isset($_POST['submit'])) {
   }
 }
 ?>
+</body>
 
 </html>
